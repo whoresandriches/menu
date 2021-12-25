cd1d28c67a2047d37caae37a18c3e97cedd0f8fb
hagodinero <goodkidparker@gmail.com>
2021-12-24 23:40:28 -0400

    noicee

---
+++ main.js
@@ -0,0 +1,62 @@
+const copyRap = () => {
+    const copyRap = document.getElementById("rap-text");
+
+    copyRap.select();
+    copyRap.setSelectionRange(0, 99999);
+  
+    navigator.clipboard.writeText(`Hey there, I’m selling pics, videos and meetup sex. Do you want to see the menu?❤️ if you’re interested in a meetup may I know where are you based?`);
+}
+
+
+const copyMenu = () => {
+    const copyMenu = document.getElementById("menu-text");
+
+    copyMenu.select();
+    copyMenu.setSelectionRange(0, 99999);
+  
+    navigator.clipboard.writeText(`$120 Condom sex one hour
+
+$150 Raw sex no condom (creampie ) one hour 
+    
+$200 Sex and anal sex included one hour 
+If you want an extra hour that’d be $50 more!
+    
+(oral is included in all of this meets because what’s great sex without oral🥰) `);
+}
+
+
+const copyZelle = () => {
+    const copyZelle = document.getElementById("zelle-text");
+
+    copyZelle.select();
+    copyZelle.setSelectionRange(0, 99999);
+  
+    navigator.clipboard.writeText(copyZelle.value);
+}
+
+const copyCash = () => {
+    const copyCash = document.getElementById("cashapp-text");
+
+    copyCash.select();
+    copyCash.setSelectionRange(0, 99999);
+  
+    navigator.clipboard.writeText(copyCash.value);
+}
+
+const copyVenmo = () => {
+    const copyVenmo = document.getElementById("venmo-text");
+
+    copyVenmo.select();
+    copyVenmo.setSelectionRange(0, 99999);
+  
+    navigator.clipboard.writeText(copyVenmo.value);
+}
+
+const copyPaypal = () => {
+    const copyPaypal = document.getElementById("Paypal-text");
+
+    copyPaypal.select();
+    copyPaypal.setSelectionRange(0, 99999);
+  
+    navigator.clipboard.writeText(copyPaypal.value);
+}
